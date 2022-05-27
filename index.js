0480c3cf8279689f7bf22734d37b38a542133405
Nahid Hasan <nahidhasankiran2@gmail.com>
2022-05-27 10:36:15 +0600

    Products added in db

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
-const express = require('express')
-const cors = require("cors")
-const app = express()
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hola! Server working perfectly')
-})
+// Use Express
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.klt1tc5.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const partsCollection = client.db("lampzone").collection("parts");
+    app.get("/parts",async(req,res) => {
+      const query = {};
+      const cursor = partsCollection.find(query);
+      const parts = await cursor.toArray();
+      res.send(parts)
+    });
+  }
+  finally {
+
+  }
+}
+run().catch(console.dir)
+
+app.get("/", (req, res) => {
+  res.send("Hola! Server working perfectly");
+});
 
 app.listen(port, () => {
-  console.log(`Lamp zone running from port: ${port}`)
-})
+  console.log(`Lamp zone running from port: ${port}`);
+});
